43c88c64669659c0e2db15d14d0dba65193f71c2
YW Z <32604270+echoztoronto@users.noreply.github.com>
2019-02-01 23:12:44 -0500

    added slide show

--- javascript.js
+++ javascript.js
@@ -12,3 +12,43 @@ function showdemo(objstr) {
 }
 
 
+//Slide show swap
+var slidenum = 1;
+var total = 6;
+
+function slideswap(n) {
+    slidenum = slidenum + n;
+    if (slidenum === total + 1) {
+        slidenum = 1;
+    } 
+    if (slidenum === 0) {
+        slidenum = total;
+    }
+
+    switch(slidenum) {
+        case 1:
+        document.getElementById("slide_image").src="/image/gujian.jpg";
+        document.getElementById("slide_cap").textContent="Edited Screenshot of Gu Jian 3";
+        break;
+        case 2:
+        document.getElementById("slide_image").src="/image/gujian2.jpg";
+        document.getElementById("slide_cap").textContent="Edited Screenshot of Gu Jian 3";
+        break;
+        case 3:
+        document.getElementById("slide_image").src="/image/elderscroll.jpg";
+        document.getElementById("slide_cap").textContent="Screenshot of The Elder Scrolls V";
+        break;
+        case 4:
+        document.getElementById("slide_image").src="/image/elderscroll2.jpg";
+        document.getElementById("slide_cap").textContent="Screenshot of The Elder Scrolls V";
+        break;
+        case 5:
+        document.getElementById("slide_image").src="/image/elderscroll3.jpg";
+        document.getElementById("slide_cap").textContent="Screenshot of The Elder Scrolls V";
+        break;
+        case 6:
+        document.getElementById("slide_image").src="/image/bird.jpg";
+        document.getElementById("slide_cap").textContent="A photo I took in 2012";
+        break;        
+    }
+}
